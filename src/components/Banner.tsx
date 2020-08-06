f4fc6c67855d9ea85c7eae25987c86301a016f8a
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-06 15:17:56 -0700

    Cleaning code on Banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-interface Props {
+interface bannerProps {
     bannerImg: string;
+    altImg: string;
 }
-const Banner: React.FC<Props> = ({ bannerImg }) => {
+const Banner: React.FC<bannerProps> = ({ bannerImg, altImg }) => {
     return (
         <div>
-            <img src={bannerImg} />
+            <img src={bannerImg} alt={altImg} />
         </div>
     );
 };
 
 Banner.propTypes = {
     bannerImg: PropTypes.string.isRequired,
+    altImg: PropTypes.string.isRequired,
 };
 export default Banner;
