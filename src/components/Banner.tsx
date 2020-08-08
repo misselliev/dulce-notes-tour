aee7ef6f045a459f9690f58961c305ca62843bd9
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-07 19:53:55 -0700

    Fixing Pascal Casing in Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-interface bannerProps {
-    bannerImg: string;
-    altImg: string;
+interface BannerProps {
+    BannerImg: string;
+    AltImg: string;
 }
-const Banner: React.FC<bannerProps> = ({ bannerImg, altImg }) => {
+const Banner: React.FC<BannerProps> = ({ BannerImg, AltImg }) => {
     return (
-        <div>
-            <img src={bannerImg} alt={altImg} />
+        <div className="flex w-full h-full">
+            <div className="container mx-auto rounded-t shadow bg-cover bg-bottom m-4">
+                <img src={BannerImg} alt={AltImg} />
+            </div>
         </div>
     );
 };
 
 Banner.propTypes = {
-    bannerImg: PropTypes.string.isRequired,
-    altImg: PropTypes.string.isRequired,
+    BannerImg: PropTypes.string.isRequired,
+    AltImg: PropTypes.string.isRequired,
 };
 export default Banner;
