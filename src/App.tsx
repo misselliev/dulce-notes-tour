a577e8dd0604f4fbd630dcfa85ed46c2e2c4288c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-12 14:40:49 -0700

    Adding routing to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Menubar from './components/Menubar';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import Routes from './Routes';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Menubar />
-            <Banner BannerImg="https://wallpaperset.com/w/full/0/7/f/32861.jpg" AltImg="The 1975 BW banner" />
-            <Footer />
+            <Router>
+                <Menubar />
+                <Banner BannerImg="https://wallpaperset.com/w/full/0/7/f/32861.jpg" AltImg="The 1975 BW banner" />
+                <Footer />
+                <Routes />
+            </Router>
         </div>
     );
 };
