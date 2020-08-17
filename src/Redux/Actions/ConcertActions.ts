8c8f72ce18473dfb35c693d4b967c6fbec8fccec
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-17 14:42:53 -0700

    Updating API url, updating TourDates with useEffect

--- src/Redux/Actions/ConcertActions.ts
+++ src/Redux/Actions/ConcertActions.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { ConcertDispatchTypes, CONCERT_LOADING, CONCERT_SUCCESS, CONCERT_FAIL } from './ConcertActionsTypes';
 
 const client_id = process.env.REACT_APP_CLIENT_ID;
-const client_secret = process.env.REACT_APP_CLIENT_SECRET;
+const client_secret = process.env.REACT_APP_SECRET;
 export const getConcert = () => async (dispatch: Dispatch<ConcertDispatchTypes>) => {
     try {
         dispatch({
@@ -11,7 +11,7 @@ export const getConcert = () => async (dispatch: Dispatch<ConcertDispatchTypes>)
         });
 
         const res = await axios.get(
-            `https://api.seatgeek.com/2/events?performers.slug=the-1975&client_id=${client_id}client_secret=${client_secret}`,
+            `https://api.seatgeek.com/2/events?performers.slug=the-1975&client_id=${client_id}&client_secret=${client_secret}`,
         );
 
         dispatch({
--- src/pages/TourDates.tsx
+++ src/pages/TourDates.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DateCard from '../components/DateCard';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootStore } from '../Redux/store';
+import { getConcert } from '../Redux/Actions/ConcertActions';
 
 const TourDates: React.FC = () => {
     const concertState = useSelector((state: RootStore) => state.concerts);
     console.log(concertState);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getConcert());
+    }, [dispatch]);
 
     return (
         <div className="container mx-auto">
